dfc2bceb4155f0413f460d76675548257a23140e
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-15 00:30:46 +0000

    homeTab.js: Added restaurant name element to content

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,6 +1,8 @@
 export default function homePage() {
     const content = document.getElementById("content");
     content.innerHTML = "";
-    
+    const title = document.createElement("h1");
+    title.textContent = "Let Him Cook";
+    content.appendChild(title);
 }
 
