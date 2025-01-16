b3b1ab5f0316f735628a1cf79fbd03cf99ae6068
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-16 00:37:05 +0000

    index.js: Called homePage function when home nav is clicked

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import homePage from "./homeTab";
 import menuPage from "./menuTab";
 import "./style.css";
 homePage();
+const homeButton = document.getElementById("home");
+homeButton.addEventListener("click", homePage());
