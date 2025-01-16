0bbaf44ed06953228aa27c3c29d78ca51a387956
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-16 00:21:43 +0000

    homeTab.js: Created description, hours and address elements

--- src/homeTab.js
+++ src/homeTab.js
@@ -2,6 +2,9 @@ export default function homePage() {
     const content = document.getElementById("content");
     content.innerHTML = "";
     const title = document.createElement("h1");
+    const description = document.createElement("div");
+    const hours = document.createElement("div");
+    const address = document.createElement("div");
     title.textContent = "Let Him Cook";
     content.appendChild(title);
 }
