192f3cc015edc646c96548ab2dba4a3591d10309
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-16 23:00:54 +0000

    index.js: Added event handling to Contact nav. Now clicking calls contactPage function

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import "./style.css";
 homePage();
 const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
+const contactButton = document.getElementById("contact")
 homeButton.addEventListener("click", homePage);
 menuButton.addEventListener("click", menuPage);
+contactButton.addEventListener("click", contactPage);
